4be35fb3788a5cda4e83b66d61c583a43060f54a
15039836 <15039836@myrp.edu.sg>
2017-05-23 13:03:14 +0800

    Initial commit

---
+++ app/src/main/java/android/myapplicationdev/com/p05_ndpsongs/Song.java
@@ -0,0 +1,38 @@
+package android.myapplicationdev.com.p05_ndpsongs;
+
+import java.io.Serializable;
+
+public class Song implements Serializable {
+    private int id;
+    private String title;
+    private String singers;
+    private int year;
+    private int stars;
+
+    public Song(String title, String singers, int year, int stars) {
+        this.title = title;
+        this.singers = singers;
+        this.year = year;
+        this.stars = stars;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSingers() {
+        return singers;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+}
